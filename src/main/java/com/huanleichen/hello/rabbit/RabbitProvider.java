f6e30e14b68eca7a387b9e39a0b2c97cec553d6c
LikeDrinkingWater_glb <LikeDrinkingWater_glb@chl.com>
2019-05-08 10:30:46 +0800

    rabbitmq

---
+++ src/main/java/com/huanleichen/hello/rabbit/RabbitProvider.java
@@ -0,0 +1,18 @@
+package com.huanleichen.hello.rabbit;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class RabbitProvider {
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    public void send() {
+        String content = "Hello" + new Date();
+        amqpTemplate.convertAndSend("helloRabbit", content);
+    }
+}
---
+++ src/test/java/com/huanleichen/hello/rabbit/HelloRabbitApplicationTests.java
@@ -0,0 +1,22 @@
+package com.huanleichen.hello.rabbit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HelloRabbitApplication.class)
+public class HelloRabbitApplicationTests {
+    @Autowired
+    private RabbitProvider rabbitProvider;
+
+    @Test
+    public void contextLoads() {
+        for (int i = 0; i < 100; i++) {
+            rabbitProvider.send();
+        }
+    }
+
+}
